1eac30c69c41dc39a4f3f5bad30968e35255e529
Andy Ahn <andy@Andys-MacBook-Pro.local>
2018-09-17 15:29:36 -0700

    I added more validation on first question in cliQuestions.js. The validation checks if a user typed his/her project name with only spaces or trailing whitespaces. If the user typed the project name with spaces only, same question should be asked again; if whitespaces exist, they will be trimmed

--- cli/cliMain.js
+++ cli/cliMain.js
@@ -7,23 +7,30 @@ const cliQuestions = require('./cliQuestions.js');
 const answersList = [];
 
 
-//inquirer.registerPrompt('checkbox-plus', require('inquirer-checkbox-plus-prompt')); // regiesters checkboxPlus plugin allowing pretty drop down menus
+// inquirer.registerPrompt('checkbox-plus', require('inquirer-checkbox-plus-prompt')); // regiesters checkboxPlus plugin allowing pretty drop down menus
 
-//console.log(figlet.fontsSync()); // gives list of available fonts
+// console.log(figlet.fontsSync()); // gives list of available fonts
 
-//chalk adds color and weight ton cli fonts
-console.log(chalk.red.bgYellow.bold(figlet.textSync('React First', {
+// chalk adds color and weight ton cli fonts
+console.log(chalk.rgb(46, 255, 0).bgBlack.bold(figlet.textSync('React First', {
   // chalk is setting the color etc. of large text and background, figlet is making the large text
-  font: 'Doom',
+  font: 'Big',
   horizontalLayout: 'default',
   verticalLayout: 'default',
 })));
 
 inquirer.prompt(cliQuestions.getProjectInfo).then((answers) => {
-  const fileName = `${answers.projectName}.json`; //appends json to the name of project that the user specified
+  // removes whitespace of the answer
+  answers.projectName = answers.projectName.trim();
+
+  // appends json to the name of project that the user specified
+  const fileName = `${answers.projectName}.json`;
+
   answersList.push(answers);
+  // console.log(answers);
+
   if (answers.testFileSave === 'YES') {
-    //writes the file to disk if answer is yes+
+    // writes the file to disk if answer is yes+
     fs.writeFile(fileName, JSON.stringify(answersList[0], '', 2), 'utf8', (err) => {
       if (err) throw err;
       console.log('File Saved!');
--- cli/cliQuestions.js
+++ cli/cliQuestions.js
@@ -4,12 +4,19 @@ const cliQuestions = {};
 
 cliQuestions.getProjectInfo = [
   {
-    type: 'input', //type designates what type of prompt the user sees, creates a prompt that takes text as an answer
+    type: 'input', // type designates what type of prompt the user sees, creates a prompt that takes text as an answer
     name: 'projectName', // name is the property the answer to this prompt is saved as
     message: 'Input your project name', // message is what the user sees on screen
+    validate(answer) {
+      // if a user typed nothing or only spaces, make the user type again
+      if (answer.length === 0 || answer.trim().length === 0) {
+        return 'You must type your project name';
+      }
+      return true;
+    },
   },
   {
-    type: 'checkbox',  //creates a list of items that can be toggled on or off
+    type: 'checkbox', // creates a list of items that can be toggled on or off
     message: 'What features would you like added to your project?',
     name: 'features',
     choices: [
@@ -24,10 +31,16 @@ cliQuestions.getProjectInfo = [
         name: 'CodeSplitting with React Router',
       },
     ],
-
+    validate(answer) {
+      // checking to see if the user selected a drop down menu
+      if (answer.length === 0) {
+        return 'You must make a selection';
+      }
+      return true;
+    },
   },
   {
-    type: 'list',  //creates a selectable list of answers that is selectable with the arrow keys
+    type: 'list', // creates a selectable list of answers that is selectable with the arrow keys
     message: 'What functionality would you like to add to your project?',
     name: 'mainFunctionalitySelector',
     choices: [
@@ -41,13 +54,6 @@ cliQuestions.getProjectInfo = [
         name: 'ServiceWorker caching for offline functionality',
       },
     ],
-    validate(answer) {
-      // checking to see if the user selected a drop down menu
-      if (answer.length === 0) {
-        return 'You must make a selection.';
-      }
-      return true;
-    },
   },
   {
     type: 'list',
